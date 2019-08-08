0e9ab036b1ae0def240688b2b5f834de60f4982a
stealpattan <stealpattan@gmail.com>
2019-08-08 20:10:19 +0900

    スマホ画面への表示に成功

--- app/src/main/java/shomama/yoursongmanager/FrameMain.java
+++ app/src/main/java/shomama/yoursongmanager/FrameMain.java
@@ -7,7 +7,7 @@ public class FrameMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frame_main);
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_frame_main);
     }
 }
---
+++ app/src/main/java/shomama/yoursongmanager/sampleMain.kt
@@ -0,0 +1,11 @@
+package shomama.yoursongmanager
+
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+
+class sampleMain : FragmentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_main)
+    }
+}
