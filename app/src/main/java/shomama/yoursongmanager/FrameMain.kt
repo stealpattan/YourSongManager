140dc14b216d946d19d95889cd8b82aa04c6eb9b
stealpattan <stealpattan@gmail.com>
2020-03-01 01:17:04 +0900

    set listview widget

---
+++ app/src/main/java/shomama/yoursongmanager/FrameMain.kt
@@ -0,0 +1,12 @@
+package shomama.yoursongmanager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FrameMain : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_main)
+    }
+}
