417dcc6745b215b7b9605c779435f41f207eff31
stealpattan <stealpattan@gmail.com>
2020-03-01 14:27:06 +0900

    write code about listview widget

--- app/src/main/java/shomama/yoursongmanager/FrameMain.kt
+++ app/src/main/java/shomama/yoursongmanager/FrameMain.kt
@@ -3,10 +3,22 @@ package shomama.yoursongmanager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+// widget libs
+import android.widget.ListView
+import android.widget.AdapterView
+
+
 class FrameMain : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame_main)
+
+        // widget initialize
+        val musicListView = findViewById(R.id.musicList) as ListView
+
+        val array = arrayOf("shomama", "shomama2", "shomama3") as Array<String>
+
+
     }
 }
