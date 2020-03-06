9ecee7ea484cb4c6f89226dc73268031fd9ead39
Ito Shoma <stealpattan@gmail.com>
2020-03-06 18:53:36 +0900

    before test. func -> listview test

--- app/src/main/java/shomama/yoursongmanager/FrameMain.kt
+++ app/src/main/java/shomama/yoursongmanager/FrameMain.kt
@@ -1,15 +1,17 @@
 package shomama.yoursongmanager
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 // widget libs
 import android.widget.ListView
-import android.widget.AdapterView
+import android.widget.ArrayAdapter
 
 
 class FrameMain : AppCompatActivity() {
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame_main)
@@ -17,8 +19,10 @@ class FrameMain : AppCompatActivity() {
         // widget initialize
         val musicListView = findViewById<ListView>(R.id.musicList)
 
-        val array = arrayOf("shomama", "shomama2", "shomama3") as Array<String>
+        val array = arrayOf("shomama", "shomama2", "shomama3")
 
-        
+        val musicListAdapter = ArrayAdapter(this, R.id.musicList, array)
+
+        musicListView.adapter = musicListAdapter
     }
 }
